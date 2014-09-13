7e5b46168aa67ce7486bc026ee500633e307f52e
Falk <falk@beamremit.com>
2014-09-13 20:28:12 +0000

    more pricing test cases

--- pricing/tests.py
+++ pricing/tests.py
@@ -17,6 +17,13 @@ class PricingTests(BeamAPITestCase):
         response = self.client.post(self.url_add, {'markup': 0.3, 'ghs_usd': 3.54})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_add_fail(self):
+        token, _ = self._create_admin_user()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post(self.url_add, {'markup': 1.3, 'ghs_usd': 3.54})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['markup'][0], 'markup has to be a value between 0 and 1')
+
     def test_get_current(self):
         token, _ = self._create_admin_user()
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
@@ -47,3 +54,13 @@ class PricingTests(BeamAPITestCase):
             response = self.client.get(reverse(self.plain_url_get_detail, args=(ids[i - 1],)))
             self.assertEqual(response.data['markup'], float(i) / 10)
             self.assertEqual(response.data['ghs_usd'], i)
+
+    def test_get_detail_fail(self):
+        token, _ = self._create_admin_user()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        response = self.client.post(self.url_add, {'markup': 0.1, 'ghs_usd': 1})
+        id = response.data['id']
+        response = self.client.get(reverse(self.plain_url_get_detail, args=(id + 1,)))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data['detail'], 'Not found')
