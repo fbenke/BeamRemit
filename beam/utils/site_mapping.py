0080aa442fcc99c1897a8c9b2c1d973036c5ebd8
Falk <falk@beamremit.com>
2014-10-27 19:39:55 +0000

    activate email adjusted to different sites

--- beam/utils/site_mapping.py
+++ beam/utils/site_mapping.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 
+from beam.utils.logging import log_error
+
 
 def get_site_by_request(request):
 
     if settings.ENV == settings.ENV_LOCAL:
         return Site.objects.get_current()
-
-    host = request.META.get('HTTP_REFERER').replace(settings.PROTOCOL).replace('/', '').replace(':', '')
-    print host
-    return Site.objects.get(domain__iexact=host)
+    try:
+        host = request.META.get('HTTP_REFERER').replace(settings.PROTOCOL, '').replace('/', '').replace(':', '')
+        print host
+        return Site.objects.get(domain__iexact=host)
+    except Site.DoesNotExist:
+        message = 'ERROR - Site Mapping: Cannot associate {} with a Site'.format(host)
+        log_error(message)
+        return Site.objects.get_current()
