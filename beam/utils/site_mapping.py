c1e9a6512c8c8540df14fcbb8da52500a8373055
Falk <falk@beamremit.com>
2014-10-27 19:28:21 +0000

    activate email adjusted to different sites

--- beam/utils/site_mapping.py
+++ beam/utils/site_mapping.py
@@ -3,5 +3,10 @@ from django.contrib.sites.models import Site
 
 
 def get_site_by_request(request):
+
+    if settings.ENV == settings.ENV_LOCAL:
+        return Site.objects.get_current()
+
     host = request.META.get('HTTP_REFERER').replace(settings.PROTOCOL, '').replace('/', '')
+    print host
     return Site.objects.get(domain__iexact=host)
