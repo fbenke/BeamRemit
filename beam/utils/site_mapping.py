1d2f18847b04e67b1f360dbec2ec4a423bcd98b1
Falk <falk@beamremit.com>
2014-10-27 19:22:25 +0000

    activate email adjusted to different sites

--- beam/utils/site_mapping.py
+++ beam/utils/site_mapping.py
@@ -1,14 +1,7 @@
+from django.conf import settings
 from django.contrib.sites.models import Site
 
 
 def get_site_by_request(request):
-    '''
-    Simple Rewrite of the function models._get_site_by_request in Django 1.7
-    https://github.com/django/django/blob/master/django/contrib/sites/models.py
-    '''
-
-    # TODO: After upgrade to Django 1.7 this should be solved directly byusing the Django Sites Framework
-    host = request.get_host()
-    print host
-    print request.META.get('HTTP_REFERER')
+    host = request.META.get('HTTP_REFERER').replace(settings.PROTOCOL, '').replace('/', '')
     return Site.objects.get(domain__iexact=host)
